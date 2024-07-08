52ae4184856a9f01bfe27ce49c2f6870c91ffc3b
kelvinace <salamisamod5@gmail.com>
2024-07-08 10:37:03 +0100

    ds

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Exit immediately if a command exits with a non-zero status
 set -e
-
+python3.9 -m venv venv
 source venv/bin/activate 
 # Initialize and update git submodules (if any)
 git submodule init

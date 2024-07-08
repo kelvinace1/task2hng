1c5f726150bbb302e11265dcfce383c0b8e32e67
kelvinace <salamisamod5@gmail.com>
2024-07-08 22:13:54 +0100

    try1

--- hngorg/hngorg/settings.py
+++ hngorg/hngorg/settings.py
@@ -101,18 +101,8 @@ SIMPLE_JWT = {
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT"),
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
-
-
-DATABASES['default'] = dj_database_url.config(default=os.getenv('DATABASE_URL'))
 
 """POSTGRES_LOCALLY = False
 if ENVIRONMENT == 'production' or POSTGRES_LOCALLY == True:

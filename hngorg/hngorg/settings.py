16cb8e4f73ee230701e2ea15cbb31119709ced4f
kelvinace <salamisamod5@gmail.com>
2024-07-09 00:57:46 +0100

    hoope

--- hngorg/hngorg/settings.py
+++ hngorg/hngorg/settings.py
@@ -153,9 +153,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
 
 MEDIA_URL = '/media/'
 

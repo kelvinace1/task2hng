1c88f093c0645f665d2c5de4e6c08df51b4cce46
kelvinace <salamisamod5@gmail.com>
2024-07-08 22:53:14 +0100

    try

--- hngorg/hngorg/settings.py
+++ hngorg/hngorg/settings.py
@@ -156,6 +156,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 

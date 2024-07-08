9dfbb3d0abe955c1f73803984414234d76d1bb34
kelvinace <salamisamod5@gmail.com>
2024-07-08 11:59:06 +0100

    hope8

--- build.sh
+++ build.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 # Exit immediately if a command exits with a non-zero status
+set -e
 
 
 # Corrected path with backslashes for Windows
 
-
+source C:\Users\USER\hngorg\venv\Scripts\activate
 # Initialize and update git submodules (if any)
 export DJANGO_SETTINGS_MODULE=hngorg.settings.production
 
 
 echo "building the project"
-python3.9 -m pip install -r requirements.txt
+python3.9 pip install -r requirements.txt
 
 echo "make migrations..."
 python manage.py makemigrations --noinput

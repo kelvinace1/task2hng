b2d7dde0d3190e1afbcc010dc1405782795e4742
kelvinace <salamisamod5@gmail.com>
2024-07-08 11:14:24 +0100

    s

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 
 
 # Corrected path with backslashes for Windows
-#source C:\\Users\\USER\\hngorg\\venv\\Scripts\\activate
+
 source venv/Scripts/activate
 # Initialize and update git submodules (if any)
 export DJANGO_SETTINGS_MODULE=hngorg.settings.production

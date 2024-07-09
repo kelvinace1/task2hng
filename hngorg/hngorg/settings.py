d479b26ac1e2ebc44c3ab0e076d68e280e873730
kelvinace <salamisamod5@gmail.com>
2024-07-09 01:20:25 +0100

    lolo

--- hngorg/hngorg/settings.py
+++ hngorg/hngorg/settings.py
@@ -154,7 +154,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', )
 
 
 MEDIA_URL = '/media/'
